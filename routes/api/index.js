5f1230a48326e0f3d22ddc917d7b42d08ec7fbf6
India Amos <india@indiamos.com>
2017-06-28 12:40:06 -0400

    Start splitting API into a separate routing process

---
+++ routes/api/index.js
@@ -0,0 +1,23 @@
+var Promise = require('bluebird');
+var api_router = require('express').Router();
+var Hotel = require('../../models').Hotel;
+var Restaurant = require('../../models').Restaurant;
+var Activity = require('../../models').Activity;
+
+api_router.get('/options', function(req, res, next) {
+ Promise.all([
+    Hotel.findAll(),
+    Restaurant.findAll(),
+    Activity.findAll()
+  ])
+  .spread(function(dbHotels, dbRestaurants, dbActivities) {
+    res.json('index', {
+      templateHotels: dbHotels,
+      templateRestaurants: dbRestaurants,
+      templateActivities: dbActivities
+    });
+  })
+  .catch(next);
+});
+
+module.exports = api_router;
--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,12 @@ var router = require('express').Router();
 var Hotel = require('../models').Hotel;
 var Restaurant = require('../models').Restaurant;
 var Activity = require('../models').Activity;
+var api_router = require('./api');
 
 router.get('/', function(req, res, next) {
-  Promise.all([
-    Hotel.findAll(),
-    Restaurant.findAll(),
-    Activity.findAll()
-  ])
-  .spread(function(dbHotels, dbRestaurants, dbActivities) {
-    res.render('index', {
-      templateHotels: dbHotels,
-      templateRestaurants: dbRestaurants,
-      templateActivities: dbActivities
-    });
-  })
-  .catch(next);
+  res.render('index')
 });
+
+router.use('/api', api_router);
 
 module.exports = router;
