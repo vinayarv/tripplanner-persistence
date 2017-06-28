c8b78980012bd668f9b6d45c6c2759611a34e646
India Amos <india@indiamos.com>
2017-06-28 17:16:55 -0400

    Fix days router

--- routes/api/days.js
+++ routes/api/days.js
@@ -5,33 +5,54 @@ var Restaurant = require('../../models').Restaurant;
 var Activity = require('../../models').Activity;
 var Day = require('../../models').Day;
 
-
-day_router.get('/days', function(req, res, next) {
- Day.findAll()
-  .then(function(dbDays) {
-    res.send({
-      templateDays: dbDays
-    });
-  })
-  .catch(next);
-});
-
-day_router.get('/days/:dayNumber', function (req, res, next){
-  Day.findOne({where: {number: req.params.dayNumber}})
-  .then(function(day) {
-    res.send({
-      templateDay: day
+day_router.get('/', function(req, res, next) {
+  Day.findAll()
+    .then(function(dbDays) {
+      res.send({
+        templateDays: dbDays
+      });
     })
-  })
+    .catch(next);
 });
 
-day_router.delete('/days/:dayNumber', function (req, res, next){
-  Day.destroy({where: {number: req.params.dayNumber}});
+day_router.get('/:dayNumber', function(req, res, next) {
+  Day.findOne({ where: { number: req.params.dayNumber } })
+    .then(function(day) {
+      res.send({
+        templateDay: day
+      })
+    })
+    .catch(next);
+});
+
+day_router.delete('/:dayNumber', function(req, res, next) {
+  Day.destroy({ where: { number: req.params.dayNumber } })
+    .catch(next);
 })
 
-day_router.post('/days', function (req, res, next){
+day_router.put('/:dayNumber/:type', function(req, res, next) {
+  Day.findOne({ where: { number: req.params.dayNumber } })
+    .then(foundDay => {
+      if (req.params.type === 'hotel') {
+        // if there's no hotel yet, add one. Otherwise, replace or delete?
+      } else if (req.params.type === 'activity') {
+        // get the day id from foundDay and then look up the activity link in dayActivity. Either add or delete a row?
+      } else {
+        // type is restaurant, so get the day id from foundDay and then look up the restaurant link in dayRestaurant. Either add or delete a row?
+      }
+      res.send({
+        templateDay: day
+      })
+    })
+    .catch(next);
+})
+
+day_router.post('/', function(req, res, next) {
   Day.create()
-  .then(function(bool){return });
+    .then(function(bool) {
+      return
+    })
+    .catch(next);
 })
 
 module.exports = day_router;
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ var Restaurant = require('../models').Restaurant;
 var Activity = require('../models').Activity;
 var api_router = require('./api');
 
+router.use('/api', api_router);
+
 router.get('/', function(req, res, next) {
   res.render('index')
 });
-
-router.use('/api', api_router);
 
 module.exports = router;
